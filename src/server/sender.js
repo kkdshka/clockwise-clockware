df31227fb66ebcda8301210b95f574663ea551f8
Ksenia Kiselyova <kkdshka@gmail.com>
2018-03-29 14:46:30 +0300

    Emails sends.

--- src/server/sender.js
+++ src/server/sender.js
@@ -6,18 +6,16 @@ function sendEmail(receiverEmail, reservationData) {
         + ' в ' + reservationData.time + '.';
     nodemailer.createTestAccount((err, account) => {
         let transporter = nodemailer.createTransport({
-            host: 'smtp.ethereal.email',
-            port: 587,
-            secure: false, // true for 465, false for other ports
+            service: 'gmail',
             auth: {
-                user: account.user, // generated ethereal user
-                pass: account.pass // generated ethereal password
+                user: 'cklockware@gmail.com',
+                pass: 'passwordsecret'
             }
         });
 
         // setup email data with unicode symbols
         let mailOptions = {
-            from: 'admin@example.com', // sender address
+            from: 'Clockwise Clockware', // sender address
             to: receiverEmail, // list of receivers
             subject: 'Clockwise clockware', // Subject line
             text: text, // plain text body
@@ -29,7 +27,7 @@ function sendEmail(receiverEmail, reservationData) {
                 return console.log(error);
             }
             console.log('Message sent: %s', info.messageId);
-            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+            // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
             // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
             // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
