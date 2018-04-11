e2bb7cd5ff3ed5441fba89dbaa7f8eea6140705b
Ksenia Kiselyova <kkdshka@gmail.com>
2018-04-11 19:14:08 +0300

    Changes for heroku.

--- src/server/database.js
+++ src/server/database.js
@@ -2,12 +2,12 @@ const mysql = require('mysql');
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
+    host: 'db4free.net',
     acquireTimeout: 30000,
     port: 3306,
-    user: 'admin',
-    password: 'passwordsecret',
-    database: 'test'
+    user: 'kkdshka',
+    password: '6e7c9f',
+    database: 'kkdshka_test'
 });
 
 module.exports = pool;
