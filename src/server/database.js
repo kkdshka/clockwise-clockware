10853bdfa3a669f74d0b352d0abc05501d30ae0f
Ksenia Kiselyova <kkdshka@gmail.com>
2018-04-12 13:49:30 +0300

    Small changes.

--- src/server/database.js
+++ src/server/database.js
@@ -2,12 +2,12 @@ const mysql = require('mysql');
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'db4free.net',
+    host: 'localhost',
     acquireTimeout: 30000,
     port: 3306,
-    user: 'kkdshka',
-    password: '6e7c9f',
-    database: 'kkdshka_test'
+    user: 'admin',
+    password: 'passwordsecret',
+    database: 'test'
 });
 
 module.exports = pool;
