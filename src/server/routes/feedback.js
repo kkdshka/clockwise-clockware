20dfd5bd0a7cfbd3924190db85baef1f6a3d5985
Ksenia Kiselyova <kkdshka@gmail.com>
2018-06-11 17:08:14 +0300

    Small fixes.

--- src/server/routes/feedback.js
+++ src/server/routes/feedback.js
@@ -5,14 +5,20 @@ const tokenLifetimeRepository = require('../repositories/tokenLifetimesRepositor
 const moment = require('moment');
 
 router.get('/', async function (req, res) {
-    const token = await tokenLifetimeRepository.getTokenLifetime(req.query.token);
+    try {
+        const token = await tokenLifetimeRepository.getTokenLifetime(req.query.token);
 
-    if (moment(token.lifetime_end).isAfter(moment())) {
-        res.sendFile(path.join(__dirname, "../../../index.html"));
-    }
-    else {
+        if (moment(token.lifetime_end).isAfter(moment())) {
+            res.sendFile(path.join(__dirname, "../../../index.html"));
+        }
+        else {
+            res.sendStatus(404);
+        }
+    } catch (error) {
         res.sendStatus(404);
+        console.log(error)
     }
+
 });
 
 module.exports = router;
