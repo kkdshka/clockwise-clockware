4aa6fffff26fe4e6a72e7a5f534487ca88c90208
Ksenia <kkdshka@gmail.com>
2018-06-28 14:06:29 +0300

    Fixed alter column migration with constraint.

--- migrations/19-change_city_id_column_in_city_translations.js
+++ migrations/19-change_city_id_column_in_city_translations.js
@@ -1,24 +1,30 @@
 'use strict';
 module.exports = {
     up: (queryInterface, Sequelize) => {
-        return queryInterface.changeColumn('city_translations', 'city_id', {
-                onDelete: "cascade",
-                type: Sequelize.INTEGER,
-                references: {
-                    model: 'cities',
-                    key: 'id'
-                },
-            }
-        );
+        return queryInterface.removeConstraint('city_translations', 'city_translations_city_id_foreign_idx')
+            .then(() => {
+                return queryInterface.changeColumn('city_translations', 'city_id', {
+                        onDelete: "cascade",
+                        type: Sequelize.INTEGER,
+                        references: {
+                            model: 'cities',
+                            key: 'id'
+                        },
+                    }
+                );
+            });
     },
     down: (queryInterface, Sequelize) => {
-        return queryInterface.changeColumn('city_translations', 'city_id', {
-                type: Sequelize.INTEGER,
-                references: {
-                    model: 'cities',
-                    key: 'id'
-                },
-            }
-        );
+        return queryInterface.removeConstraint('city_translations', 'city_translations_city_id_foreign_idx')
+            .then(() => {
+                return queryInterface.changeColumn('city_translations', 'city_id', {
+                        type: Sequelize.INTEGER,
+                        references: {
+                            model: 'cities',
+                            key: 'id'
+                        },
+                    }
+                );
+            });
     }
 };
